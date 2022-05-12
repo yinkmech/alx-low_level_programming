79febd17deb738320c96be4445ba70c4ae33f261
yinkmech <80957517+yinkmech@users.noreply.github.com>
2022-05-12 01:53:31 -0700

    Update 100-main.c

--- 0x08-recursion/100-main.c
+++ 0x08-recursion/100-main.c
@@ -1,1 +1,27 @@
+Write a function that returns 1 if a string is a palindrome and 0 if not.
 
+Prototype: int is_palindrome(char *s);
+An empty string is a palindrome
+julien@ubuntu:~/0x08. Recursion$ cat 100-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
+}
